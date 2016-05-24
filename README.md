030fc922468a47704db7076ad8463a57f52be167
FlickerStudio <flickerstudiord@gmail.com>
2016-05-23 22:31:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+libstr.h
+====
+
+ Functions:
+
+ * `substr()`: return a sub-string of a string
+ * `indexOf()`: return the index of a char in a string
+ * `indexOfSub()`: return the index of a char in a sub-string
+
+LICENSE
+====
+[MIT](LICENSE)
---
+++ examples/indexofsub.c
@@ -0,0 +1,25 @@
+#include "../libstr.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /*Task: find the index of 'e' in 'I have...' */
+    char* msg = (char*)"The Life is a game and I have the ball";
+
+    /* Without indexOfSub: */
+
+    int I = indexOf('I', msg); /* I in 'I have...' */
+    int len = strlen(msg);
+    char* sentence = substr(msg, I, len); /* => I have the ball */
+    int e = indexOf('e',sentence); /* e in 'have' */
+    e = ( len - strlen(sentence) ) + e; /* => 28 */
+    printf("Index Of 'e': %d\n", e);
+
+    /* Using indexOfSub: */
+
+    I = indexOf('I', msg);
+    e = indexOfSub('e', msg, I); /* find in 'I have the ball' */
+    printf("Index Of 'e': %d\n", e); /* => 28*/
+
+    return 0;
+}
